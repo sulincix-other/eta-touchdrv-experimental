92ffcae2e666efb76528f48d4e30fe462971ed5e
sulincix <parduscix@yandex.ru>
2023-11-19 16:23:36 +0000

    more commends

--- OtdDrv.c
+++ OtdDrv.c
@@ -5,13 +5,16 @@
 #include <linux/input.h>
 #include <linux/uinput.h>
 
+// Module information
 MODULE_DESCRIPTION("USB driver for Optical touch screen");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Optical touch screen");
 
+// Driver and device information
 #define DRIVER_NAME "OpticalTouchDevice"
 #define DEVICE_NAME "Otd"
 
+// USB device IDs for supported devices
 static struct usb_device_id const dev_table[] = {
     { USB_DEVICE(0x2621, 0x2201) },
     { USB_DEVICE(0x2621, 0x4501) },
@@ -19,23 +22,26 @@ static struct usb_device_id const dev_table[] = {
 };
 MODULE_DEVICE_TABLE(usb, dev_table);
 
+// Input device structure for uinput
 static struct input_dev *uinput_dev;
 
+// Probe function called when a matching device is found
 static int otd_probe(struct usb_interface *intf, const struct usb_device_id *id)
 {
+    // Extract USB device information
     struct usb_device *udev = interface_to_usbdev(intf);
     struct usb_device_descriptor desc;
     struct usb_host_interface *interface;
     struct usb_endpoint_descriptor *endpoint;
     int i, endpoint_in = 0, endpoint_out = 0;
 
-    // Read the device descriptor
+    // Read the device descriptor to get device information
     if (usb_get_descriptor(udev, USB_DT_DEVICE, 0, &desc, sizeof(desc))) {
         printk(KERN_ERR "Failed to get device descriptor\n");
         return -ENODEV;
     }
 
-    // Find the bulk endpoints
+    // Find the bulk endpoints for communication
     interface = intf->cur_altsetting;
     for (i = 0; i < interface->desc.bNumEndpoints; i++) {
         endpoint = &interface->endpoint[i].desc;
@@ -61,9 +67,10 @@ static int otd_probe(struct usb_interface *intf, const struct usb_device_id *id)
     __set_bit(ABS_MT_POSITION_Y, uinput_dev->absbit);
     __set_bit(ABS_MT_TOUCH_MAJOR, uinput_dev->absbit);
 
-    // Set multi-touch parameters
+    // Set multi-touch parameters (manually since input_mt_init_slots is not available)
     uinput_dev->absinfo[ABS_MT_SLOT].maximum = 1;
     uinput_dev->absinfo[ABS_MT_SLOT].resolution = 0;
+    input_set_abs_params(uinput_dev, ABS_MT_SLOT, 0, 1, 0, 0);
     input_set_abs_params(uinput_dev, ABS_MT_POSITION_X, 0, 32767, 0, 0);
     input_set_abs_params(uinput_dev, ABS_MT_POSITION_Y, 0, 32767, 0, 0);
     input_set_abs_params(uinput_dev, ABS_MT_TOUCH_MAJOR, 0, 32767, 0, 0);
@@ -75,12 +82,15 @@ static int otd_probe(struct usb_interface *intf, const struct usb_device_id *id)
         return -ENOMEM;
     }
 
+    // Set a pointer to the USB device in the interface data
     usb_set_intfdata(intf, udev);
     return 0;
 }
 
+// Disconnect function called when the device is removed
 static void otd_disconnect(struct usb_interface *intf)
 {
+    // Get the USB device pointer from the interface data
     struct usb_device *udev = usb_get_intfdata(intf);
 
     // Unregister and free uinput device
@@ -89,9 +99,11 @@ static void otd_disconnect(struct usb_interface *intf)
         input_free_device(uinput_dev);
     }
 
+    // Clear the pointer to the USB device in the interface data
     usb_set_intfdata(intf, NULL);
 }
 
+// USB driver structure
 static struct usb_driver otd_driver = {
     .name = DRIVER_NAME,
     .probe = otd_probe,
@@ -99,10 +111,12 @@ static struct usb_driver otd_driver = {
     .id_table = dev_table,
 };
 
+// Module initialization function
 static int otd_init(void)
 {
     int result;
 
+    // Register the USB driver
     result = usb_register(&otd_driver);
     if (result) {
         printk(KERN_ERR "usb_register failed. Error number %d\n", result);
@@ -112,11 +126,14 @@ static int otd_init(void)
     return 0;
 }
 
+// Module exit function
 static void otd_exit(void)
 {
+    // Deregister the USB driver
     usb_deregister(&otd_driver);
 }
 
+// Specify the initialization and exit functions
 module_init(otd_init);
 module_exit(otd_exit);
 
